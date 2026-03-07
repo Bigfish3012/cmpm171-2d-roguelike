c8730156c358ab34f88a4ccec85ff36066d7033b
bigfish3012 <lichengkun0907@gmail.com>
2026-03-06 16:19:03 -0800

    update button click SFX

--- CMPM171-game/Assets/Scripts/audio/UI_SFXPlayer.cs
+++ CMPM171-game/Assets/Scripts/audio/UI_SFXPlayer.cs
@@ -4,7 +4,7 @@ public class UISFXPlayer : MonoBehaviour
 {
     [SerializeField] private AudioSource audioSource;
     [SerializeField] private AudioClip clickClip;
-    [Range(0f, 1f)][SerializeField] private float volume = 1f;
+    [Range(0f, 2f)][SerializeField] private float volume = 2f;
 
     private void Awake()
     {
